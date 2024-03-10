4e8bcb726821dc02a03b21906ceb96c1044d2b3e
Stun3 <162827122+Stun3@users.noreply.github.com>
2024-03-10 07:30:12 +0100

    Add files via upload

--- speedtest-servers-static.php
+++ speedtest-servers-static.php
@@ -1,26 +1,5 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<settings>
-<servers>
-<server url="https://speedtest6.digi.hu:8080/speedtest/upload.php" lat="47.5000" lon="19.0500" name="Budapest" country="Hungary" cc="HU" sponsor="Digi Kft" id="31717" host="speedtest6.digi.hu:8080"/>
-<server url="http://speedtest.atw.hu:8080/speedtest/upload.php" lat="47.5000" lon="19.0500" name="Budapest" country="Hungary" cc="HU" sponsor="ATW Internet Kft." id="7842" host="speedtest.atw.hu:8080"/>
-<server url="http://bpspeedtest.giganet.hu:8080/speedtest/upload.php" lat="47.5000" lon="19.0500" name="Budapest" country="Hungary" cc="HU" sponsor="Giganet Internet Kft" id="41195" host="bpspeedtest.giganet.hu:8080"/>
-<server url="http://speedtest.microsystem.hu:8080/speedtest/upload.php" lat="47.5000" lon="19.0500" name="Budapest" country="Hungary" cc="HU" sponsor="Microsystem-Kecskemét Kft" id="36406" host="speedtest.microsystem.hu:8080"/>
-<server url="http://teszt.novicom.hu:8080/speedtest/upload.php" lat="47.3667" lon="20.6347" name="Kunhegyes" country="Hungary" cc="HU" sponsor="Novi-Com Kft." id="37106" host="teszt.novicom.hu:8080"/>
-<server url="http://teszt2.novicom.hu:8080/speedtest/upload.php" lat="47.1965" lon="18.6115" name="Gárdony" country="Hungary" cc="HU" sponsor="Novi-Com Kft." id="44566" host="teszt2.novicom.hu:8080"/>
-<server url="http://speed.sajoktv.hu:8080/upload.php" lat="48.1320" lon="20.8594" name="Arnot" country="Hungary" cc="HU" sponsor="O&T Kft" id="24548" host="speed.sajoktv.hu:8080"/>
-<server url="http://speedtest.datatrans.hu:8080/speedtest/upload.php" lat="47.1956" lon="18.4089" name="Szekesfehervar" country="Hungary" cc="HU" sponsor="Datatrans Internet Kft" id="2536" host="speedtest.datatrans.hu:8080"/>
-<server url="http://speedtest.ferdonet.sk:8080/speedtest/upload.php" lat="48.6835" lon="20.1138" name="Revúca" country="Slovakia" cc="SK" sponsor="FERDINANDY s.r.o" id="31572" host="speedtest.ferdonet.sk:8080"/>
-<server url="http://speedmeter-bb.orange.sk:8080/speedtest/upload.php" lat="48.7358" lon="19.1541" name="Banska Bystrica" country="Slovakia" cc="SK" sponsor="Orange Slovakia" id="24924" host="speedmeter-bb.orange.sk:8080"/>
-<server url="http://127.0.0.1/http/upload.php" lat="46.4763" lon="15.6332" name="Localhost" country="Slovakia" cc="SI" sponsor="Localhost" id="11368"  host="127.0.0.1:80" />
-<server url="http://speedtest.nessus.at:8080/speedtest/upload.php" lat="48.2088" lon="16.3726" name="Vienna" country="Austria" cc="AT" sponsor="Nessus GmbH (10G Uplink)" id="3744" host="speedtest.nessus.at:8080"/>
-<server url="http://h3g-speed1.rootsrv.nextlayer.at:8080/speedtest/upload.php" lat="48.2088" lon="16.3726" name="Vienna" country="Austria" cc="AT" sponsor="Hutchison Drei Austria GmbH" id="32648" host="h3g-speed1.rootsrv.nextlayer.at:8080"/>
-<server url="https://speedtest.blizznet.at:8080/speedtest/upload.php" lat="48.2088" lon="16.3726" name="Vienna" country="Austria" cc="AT" sponsor="Wien Energie Business" id="62623" host="speedtest.blizznet.at:8080"/>
-<server url="http://datapacket.xyz:8080/speedtest/upload.php" lat="48.2082" lon="16.3738" name="Vienna" country="Austria" cc="AT" sponsor="DataPacket" id="50599" host="datapacket.xyz:8080"/>
-<server url="http://speedtest.fonira.at:8080/speedtest/upload.php" lat="48.2082" lon="16.3738" name="Vienna" country="Austria" cc="AT" sponsor="fonira Telekom GmbH" id="15152" host="speedtest.fonira.at:8080"/>
-<server url="http://speedmeter.orange.sk:8080/speedtest/upload.php" lat="48.1485" lon="17.1067" name="Bratislava" country="Slovakia" cc="SK" sponsor="Orange Slovakia" id="3902" host="speedmeter.orange.sk:8080"/>
-<server url="http://sp1.telekom.sk:8080/speedtest/upload.php" lat="48.1485" lon="17.1067" name="Bratislava" country="Slovakia" cc="SK" sponsor="Slovak Telekom" id="7069" host="sp1.telekom.sk:8080"/>
-<server url="http://speedtest.lightstorm.sk:8080/speedtest/upload.php" lat="48.1485" lon="17.1067" name="Bratislava" country="Slovakia" cc="SK" sponsor="LightStorm Communications s.r.o." id="29920" host="speedtest.lightstorm.sk:8080"/>
-<server url="http://speedtest.ikmg.sk:8080/speedtest/upload.php" lat="48.1485" lon="17.1067" name="Bratislava" country="Slovakia" cc="SK" sponsor="IKMG" id="21657" host="speedtest.ikmg.sk:8080"/>
-<server url="http://speedtest.slovanet.sk:8080/speedtest/upload.php" lat="48.1485" lon="17.1067" name="Bratislava" country="Slovakia" cc="SK" sponsor="Slovanet, a.s." id="30839" host="speedtest.slovanet.sk:8080"/>
-</servers>
-</settings>
+<?php
+header('Content-type: text/xml');
+$xml = file_get_contents('servers.xml');
+echo $xml;
+?>
